f7ec9020a3413ec25060ae3a52c1e8e3535b114e
prajjwalsubedi <66186489+prajjwalsubedi@users.noreply.github.com>
2022-07-28 12:28:36 +1000

    1

--- backend/views.py
+++ backend/views.py
@@ -1,5 +1,132 @@
 from django.shortcuts import render
 
 # Create your views here.
+def Calendar(request):
+    return render(request, "calendar.html")
+
+def ChartD(request):
+    return render(request, "chart-3d.html")
+def ChartChartist(request):
+    return render(request, "chart-chartist.html")
+def ChartChartjs(request):
+    return render(request, "chart-chartjs.html")
+def ChartDynamic(request):
+    return render(request, "chart-dynamic.html")
+def ChartFlot(request):
+    return render(request, "chart-flot.html")
+def ChartKnob(request):
+    return render(request, "chart-knob.html")
+def ChartMorris(request):
+    return render(request, "chart-morris.html")
+def ChartOther(request):
+    return render(request, "chart-other.html")
+def ChartSparkline(request):
+    return render(request, "chart-sparkline.html")
+def Compose(request):
+    return render(request, "compose.html")
 def Dashboard(request):
+    return render(request, "dashboard.html")
+def EmailTemplateVerify(request):
+    return render(request, "email-template-verify.html")
+def ExtrasContact(request):
+    return render(request, "extras-contact.html")
+def ExtrasEmailTemplate(request):
+    return render(request, "extras-email-template.html")
+def ExtrasFaq(request):
+    return render(request, "extras-faq.html")
+def ExtrasGallery(request):
+    return render(request, "extras-gallery.html")
+def ExtrasInvoice(request):
+    return render(request, "extras-invoice.html")
+def ExtrasMaps(request):
+    return render(request, "extras-maps.html")
+def ExtrasPricing(request):
+    return render(request, "extras-pricing.html")
+def ExtrasProjects(request):
+    return render(request, "extras-projects.html")
+def ExtrasTaskboard(request):
+    return render(request, "extras-taskboard.html")
+def ExtrasTimeline(request):
+    return render(request, "extras-timeline.html")
+def ExtrasTour(request):
+    return render(request, "extras-tour.html")
+def FormAdvanced(request):
+    return render(request, "form-advanced.html")
+def FormElements(request):
+    return render(request, "form-elements.html")
+def FormFileupload(request):
+    return render(request, "form-fileupload.html")
+def FormValidation(request):
+    return render(request, "form-validation.html")
+def FormWizard(request):
+    return render(request, "form-wizard.html")
+def FormWysiwig(request):
+    return render(request, "form-wysiwig.html")
+def FormXeditable(request):
+    return render(request, "form-xeditable.html")
+def IconsFontAwesomeIcons(request):
+    return render(request, "icons-font-awesome-icons.html")
+def IconsFontello(request):
+    return render(request, "icons-fontello.html")
+def IconsMaterialDesignIconic(request):
+    return render(request, "icons-material-design-iconic.html")
+def IconsMaterialIcons(request):
+    return render(request, "icons-material-icons.html")
+def IconsThemifyIcons(request):
+    return render(request, "icons-themify-icons.html")
+def Inbox(request):
+    return render(request, "inbox.html")
+def Index(request):
     return render(request, "index.html")
+def Page404(request):
+    return render(request, "page-404.html")
+def Page500(request):
+    return render(request, "page-500.html")
+def PageConfirmMail(request):
+    return render(request, "page-confirm-mail.html")
+def PageLockScreen(request):
+    return render(request, "page-lock-screen.html")
+def PageLogin(request):
+    return render(request, "page-login.html")
+def PageRecoverpw(request):
+    return render(request, "page-recoverpw.html")
+def PageRegister(request):
+    return render(request, "page-register.html")
+def PageStarter(request):
+    return render(request, "page-starter.html")
+def Profile(request):
+    return render(request, "profile.html")
+def ReadMail(request):
+    return render(request, "read-mail.html")
+def TablesBasic(request):
+    return render(request, "tables-basic.html")
+def TablesDatatable(request):
+    return render(request, "tables-datatable.html")
+def TablesEditable(request):
+    return render(request, "tables-editable.html")
+def TablesResponsive(request):
+    return render(request, "tables-responsive.html")
+def UiButtons(request):
+    return render(request, "ui-buttons.html")
+def UiCards(request):
+    return render(request, "ui-cards.html")
+def UiCheckboxRadio(request):
+    return render(request, "ui-checkbox-radio.html")
+def UiComponents(request):
+    return render(request, "ui-components.html")
+def UiDraggableCards(request):
+    return render(request, "ui-draggable-cards.html")
+def UiModals(request):
+    return render(request, "ui-modals.html")
+def UiNotification(request):
+    return render(request, "ui-notification.html")
+def UiRangeSlider(request):
+    return render(request, "ui-range-slider.html")
+def UiSweetalert(request):
+    return render(request, "ui-sweetalert.html")
+def UiTreeview(request):
+    return render(request, "ui-treeview.html")
+def UiTypography(request):
+    return render(request, "ui-typography.html")
+def Widgets(request):
+    return render(request, "widgets.html")
